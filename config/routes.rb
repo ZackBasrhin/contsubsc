8a70bd1257680d2dfc079778818530766c42c616
ZackBasrhin <go.my.way.0620@gmail.com>
2020-03-25 17:48:57 +0900

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # 将来的には個別ページにrootするようにする（使用者はまず最初に自分のデータを見たいから）
   root to: 'incomes#top'
-  resources :incomes, only: [:index, :new, :show, :edit] do
-    resources :fixed_costs, only: :index
+  resources :incomes, only: [:index, :new, :edit, :update, :show] do
+    resources :fixed_costs, only: [:index, :new, :edit, :update]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
