fc713f97243c68046f7dbce311f016693a9197e3
ZackBasrhin <go.my.way.0620@gmail.com>
2020-03-26 14:14:43 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'incomes#top'
-  resources :incomes, only: [:index, :new, :edit, :update, :show]
+  resources :incomes, only: [:top, :index, :new, :edit, :update, :show]
   resources :fixed_costs, only: [:index, :new, :create, :edit, :update]
   resources :users, only: [:edit, :update]
 end
